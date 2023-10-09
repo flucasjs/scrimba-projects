6a18deeca9162e23118957ff2b17cc4f9f40373c
unknown <flucasjs@gmail.com>
2023-10-08 23:45:34 -0700

    Add styles

--- projects/react/UnitConverter/index.tsx
+++ projects/react/UnitConverter/index.tsx
@@ -28,8 +28,11 @@ function UnitConverter() {
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-
-    setParsedInput(parseFloat(input.units));
+    if (!input.units) {
+      setParsedInput(1)
+    } else {
+      setParsedInput(parseFloat(input.units));
+    }
   };
 
   const handleChange: React.ChangeEventHandler<HTMLInputElement> = (e) => {
@@ -65,43 +68,47 @@ function UnitConverter() {
   }, [parsedInput]);
 
   return (
-    <div>
-      <div>
-        <h2>Metric/Imperial Unit Conversion</h2>
-        <form onSubmit={handleSubmit}>
-          <input
-            name="units"
-            type="text"
-            placeholder="1"
-            value={input.units}
-            onChange={handleChange}
-          />
-          <button type="submit">Convert</button>
-        </form>
-      </div>
-      <div>
-        <div>
-          <h2>Length (Meter/Feet)</h2>
-          <output>
-            {parsedOutput.units} {pluralizeUnits("meter", parsedOutput.units)} = {parsedOutput.feet} {pluralizeUnits("foot", parsedOutput.feet)} |{" "}
-            {parsedOutput.units} {pluralizeUnits("foot", parsedOutput.units)} = {parsedOutput.meters} {pluralizeUnits("meter", parsedOutput.meters)}
-          </output>
+    <div className="container max-w-[600px]">
+        <div className="bg-[#6943FF] px-12 py-8 flex flex-wrap justify-center">
+          <h2 className="mb-6 text-base font-extrabold tracking-wider text-center basis-full font-inter">Metric/Imperial Unit Conversion</h2>
+          <form onSubmit={handleSubmit} className="flex flex-col gap-y-6">
+            <input
+              name="units"
+              type="text"
+              maxLength={7}
+              size={7}
+              pattern="[0-9]*(?:\.(?=[0-9]+)(?<=\.)[0-9]+)?"
+              placeholder="#.##"
+              value={input.units}
+              onChange={handleChange}
+              className="py-2 font-inter text-3xl font-extrabold text-center focus:outline-none bg-[#6943FF] border-[#B295FF] border-[2px] rounded-[5px]"
+            />
+            <button className="text-sm text-[#3D3D3D] bg-[#FFFFFF] py-4 font-inter font-medium rounded-[5px]" type="submit">Convert</button>
+          </form>
         </div>
-        <div>
-          <h2>Volume (Liters/Gallons)</h2>
-          <output>
-            {parsedOutput.units} {pluralizeUnits("liter", parsedOutput.units)} = {parsedOutput.liters} {pluralizeUnits("gallon", parsedOutput.liters)} |{" "}
-            {parsedOutput.units} {pluralizeUnits("gallon", parsedOutput.units)} = {parsedOutput.gallons} {pluralizeUnits("liter", parsedOutput.gallons)}
-          </output>
+        <div className="bg-[#F4F4F4] text-[#5A537B] px-12 py-8 flex flex-col gap-y-6">
+          <div className="bg-[#FFFFFF] p-4 rounded-tl-[5px]">
+            <h2 className="color-[#5A537B] text-center font-inter font-semibold text-base tracking-wide">Length (Meter/Feet)</h2>
+            <output className="color=[#353535] text-sm">
+              {parsedOutput.units} {pluralizeUnits("meter", parsedOutput.units)} = {parsedOutput.feet} {pluralizeUnits("foot", parsedOutput.feet)} |{" "}
+              {parsedOutput.units} {pluralizeUnits("foot", parsedOutput.units)} = {parsedOutput.meters} {pluralizeUnits("meter", parsedOutput.meters)}
+            </output>
+          </div>
+          <div className="bg-[#FFFFFF] p-4 rounded-tl-[5px]">
+            <h2 className="color-[#5A537B] text-center font-inter font-semibold text-base tracking-wide">Volume (Liters/Gallons)</h2>
+            <output className="color=[#353535] text-sm">
+              {parsedOutput.units} {pluralizeUnits("liter", parsedOutput.units)} = {parsedOutput.liters} {pluralizeUnits("gallon", parsedOutput.liters)} |{" "}
+              {parsedOutput.units} {pluralizeUnits("gallon", parsedOutput.units)} = {parsedOutput.gallons} {pluralizeUnits("liter", parsedOutput.gallons)}
+            </output>
+          </div>
+          <div className="bg-[#FFFFFF] p-4 rounded-tl-[5px]">
+            <h2 className="color-[#5A537B] text-center font-inter font-semibold text-base tracking-wide">Mass (Kilograms/Pounds)</h2>
+            <output className="color=[#353535] text-sm">
+              {parsedOutput.units} {pluralizeUnits("kilo", parsedOutput.units)} = {parsedOutput.pounds} {pluralizeUnits("pound", parsedOutput.pound)} |{" "}
+              {parsedOutput.units} {pluralizeUnits("pound", parsedOutput.units)} = {parsedOutput.kilos} {pluralizeUnits("kilo", parsedOutput.kilos)}
+            </output>
+          </div>
         </div>
-        <div>
-          <h2>Mass (Kilograms/Pounds)</h2>
-          <output>
-            {parsedOutput.units} {pluralizeUnits("kilo", parsedOutput.units)} = {parsedOutput.pounds} {pluralizeUnits("pound", parsedOutput.pound)} |{" "}
-            {parsedOutput.units} {pluralizeUnits("pound", parsedOutput.units)} = {parsedOutput.kilos} {pluralizeUnits("kilo", parsedOutput.kilos)}
-          </output>
-        </div>
-      </div>
     </div>
   );
 }
